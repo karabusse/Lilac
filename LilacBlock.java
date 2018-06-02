7fdb39042d1a4dcbfbc932e90cbd93279e9af513
Kara Busse <kbusse6057@gmail.com>
2018-06-02 16:20:56 -0500

    Beginning of Lilac

---
+++ LilacBlock.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Date; //Provides date/time information
+
+public class LilacBlock {
+
+    public String hash;
+    public String previousHash;
+    private String data; //data held within block
+    private long timeStamp; //number of milliseconds since 1/1/1970
+
+    /**
+     * Lilac Block Constructor
+     * @param data
+     * @param previousHash
+     */
+    public LilacBlock(String data, String previousHash) {
+
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+    }
+
+}
